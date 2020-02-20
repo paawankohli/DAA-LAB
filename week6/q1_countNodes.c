f888fdfe77007cf0a0cf7432648d8e00934af645
paawan <paawan.kohli10@gmail.com>
2020-02-20 19:41:30 +0530

    week6

--- week6/q1_countNodes.c
+++ week6/q1_countNodes.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int max(int a, int b)
-{
-	return (a > b) ? a : b;
-}
+int opcount = 0;
+int max(int a, int b) { return (a > b) ? a : b; }
 
 typedef struct node* nodeptr;
-typedef struct node
-{
+typedef struct node {
 	int data;
 	nodeptr left, right;
 } node;
@@ -34,24 +31,18 @@ void insertTree(nodeptr *root, int data)
 	printf("\nEnter 1 to insert left of %d or 2 to insert right: ", (*root)->data);
 	int n;
 	scanf("%d", &n);
-	
+
 	if (n == 1)
 		insertTree(&(*root)->left, data);
 	else
 		insertTree(&(*root)->right, data);
 }
 
-int height(nodeptr root)
-{
-	if (root == NULL)
-		return 0;
-	
-	return 1 + max(height(root->left), height(root->right));
-}
-
 int countNodes(nodeptr root)
 {
-	if(root == NULL)
+	opcount++;
+
+	if (root == NULL)
 		return 0;
 
 	return 1 + countNodes(root->left) + countNodes(root->right);
@@ -61,18 +52,20 @@ int countNodes(nodeptr root)
 int main()
 {
 	nodeptr root = NULL;
-	
+
 	int n, m;
 	printf("Enter number of nodes: ");
 	scanf("%d", &n);
-	
+
 	for (int i = 0; i < n; i++)
 	{
 		printf("Enter value:  ");
 		scanf("%d", &m);
 		insertTree(&root, m);
 	}
-	
+
 	printf("\nThe no. of nodes of the tree is: %d", countNodes(root));
+	printf("\nOpcount is: %d", opcount);
+
 	return 0;
 }
